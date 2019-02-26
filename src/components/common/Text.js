2ea078f80796ddbbde8f4f69478ae3722fd221d8
appsols-dev <appsols.dev@gmail.com>
2019-02-26 20:44:16 +0500

    Text.js formatting

--- src/components/common/Text.js
+++ src/components/common/Text.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Text as RNText } from 'react-native';
 
-export const Text = ({ children }) => {
-  return <RNText>{children}</RNText>;
+import { Colors, GlobalStyles } from '~styles';
+
+export const Text = ({ children, color }) => {
+  const { textStyle } = GlobalStyles;
+
+  const textColor = Colors[color] || color;
+
+  return <RNText style={[textStyle, { color: textColor }]}>{children}</RNText>;
 };
