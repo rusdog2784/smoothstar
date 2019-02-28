43a492d92fb3d74e3f2ffcc3c7c16371d41caeb1
appsols-dev <appsols.dev@gmail.com>
2019-02-28 15:12:29 +0500

    Carousel added on top images of NewsListScreen

--- src/screens/NewsListScreen.js
+++ src/screens/NewsListScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Image, Dimensions, ScrollView, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 import { Text } from '~components/common';
 import { CardLI } from '~components';
@@ -17,7 +18,21 @@ class NewsListScreen extends Component {
     return (
       <ScrollView>
         <View style={styles.carouselViewStyle}>
-          <Image style={styles.imageStyle} resizeMode="cover" source={Assets.Images.testImg} />
+          <Swiper
+            dotColor="#bbb"
+            activeDotColor="#fff"
+            paginationStyle={styles.carouselPaginationStyle}
+            style={styles.carouselStyle}>
+            <View style={styles.slideStyle}>
+              <Image style={styles.imageStyle} resizeMode="cover" source={Assets.Images.testImg} />
+            </View>
+            <View style={styles.slideStyle}>
+              <Image style={styles.imageStyle} resizeMode="cover" source={Assets.Images.testImg} />
+            </View>
+            <View style={styles.slideStyle}>
+              <Image style={styles.imageStyle} resizeMode="cover" source={Assets.Images.testImg} />
+            </View>
+          </Swiper>
         </View>
         <View style={styles.carouselTextStyle}>
           <Text type={StyleTypes.title}>Test Heading</Text>
@@ -45,6 +60,7 @@ const styles = StyleSheet.create({
   carouselViewStyle: {
     height: height / 2.5,
     position: 'absolute',
+    width: '100%',
   },
   carouselTextStyle: {
     height: height / 2.5,
@@ -59,6 +75,18 @@ const styles = StyleSheet.create({
   },
   listViewStyle: {
     marginTop: height / 3,
+  },
+  carouselStyle: {
+    flex: 1,
+  },
+  slideStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ddd',
+  },
+  carouselPaginationStyle: {
+    top: height / 4.5,
   },
 });
 
