6f598fa88f82b5c405372ea6a8512b5be6fcb1fd
appsols-dev <appsols.dev@gmail.com>
2019-03-06 01:12:58 +0500

    status bar color set for android

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import { SplashScreen } from 'expo';
+import { SafeAreaView } from 'react-navigation';
 
 import reducers from '~redux/reducers';
 import RootNavigator from '~routes/RootNavigator';
+
+if (Platform.OS === 'android') {
+  SafeAreaView.setStatusBarHeight(0);
+}
 
 const STORE = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
