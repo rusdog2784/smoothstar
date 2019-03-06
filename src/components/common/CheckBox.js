f35528c550144489ccc596e4816168a462b7ce07
Daniyal Awan <daniyal.intellicel@gmail.com>
2019-03-06 14:36:56 +0500

    ChekcBox custom made

--- src/components/common/CheckBox.js
+++ src/components/common/CheckBox.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { CheckBox as CB } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Icon } from 'native-base';
 
 import { Colors } from '~styles';
 
-export const CheckBox = ({ checked, style }) => {
+export const CheckBox = ({ checked, style, color = Colors.checkBoxColor }) => {
+  const name = checked ? 'checkbox-marked-outline' : 'checkbox-blank-outline';
+
   return (
-    <CB checked={checked} style={[style, styles.checkboxStyle]} color={Colors.checkBoxColor} />
+    <TouchableOpacity>
+      <Icon style={{ color }} name={name} type="MaterialCommunityIcons" />
+    </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  checkboxStyle: {
-    borderRadius: 2,
-    borderColor: Colors.checkBoxColor,
-    alignSelf: 'flex-start',
-  },
-});
