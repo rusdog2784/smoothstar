41191b1a41a25d33ef423308da3878de6fdcc7fd
appsols-dev <appsols.dev@gmail.com>
2020-08-28 18:34:28 +0500

    Formatting

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { View, Image, Dimensions, StyleSheet } from "react-native";
-import { Container, Content } from "native-base";
-import Swiper from "react-native-swiper";
-import Moment from "moment";
-import { SplashScreen } from "expo";
-import { LinearGradient } from "expo-linear-gradient";
+import React, { Component } from 'react';
+import { View, Image, Dimensions, StyleSheet } from 'react-native';
+import { Container, Content } from 'native-base';
+import Swiper from 'react-native-swiper';
+import Moment from 'moment';
+import { SplashScreen } from 'expo';
+import { LinearGradient } from 'expo-linear-gradient';
 
-import { CustomIcon } from "~components/common";
-import { CardRI } from "~components";
-import { Assets } from "~constants";
-import { GlobalStyles, Colors } from "~styles";
+import { CustomIcon } from '~components/common';
+import { CardRI } from '~components';
+import { Assets } from '~constants';
+import { GlobalStyles, Colors } from '~styles';
 
-const { height } = Dimensions.get("window");
+const { height } = Dimensions.get('window');
 
 class HomeScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -42,8 +42,7 @@ class HomeScreen extends Component {
               autoplay
               autoplayTimeout={10}
               showsPagination={false}
-              style={styles.carouselStyle}
-            >
+              style={styles.carouselStyle}>
               <View style={styles.slideStyle}>
                 <Image
                   style={styles.imageStyle}
@@ -61,7 +60,7 @@ class HomeScreen extends Component {
             </Swiper>
           </View>
           <LinearGradient
-            colors={["transparent", "transparent", Colors.backgroundColorDark]}
+            colors={['transparent', 'transparent', Colors.backgroundColorDark]}
             style={styles.linearGradientStyle}
           />
           <View style={styles.cardsViewStyle}>
@@ -69,22 +68,17 @@ class HomeScreen extends Component {
               style={{ marginBottom: 20 }}
               imgSource={Assets.Images.initSlider2}
               heading="GETTING STARTED"
-              subText="3 x Videos"
-            >
-              New to SmoothStar? folllow our guide on how to get started by
-              expert SmoothStar coaches.
+              subText="3 x Videos">
+              New to SmoothStar? folllow our guide on how to get started by expert SmoothStar
+              coaches.
             </CardRI>
 
             <CardRI
-              onPress={() =>
-                this.props.navigation.navigate("RegistrationStack")
-              }
+              onPress={() => this.props.navigation.navigate('RegistrationStack')}
               imgSource={Assets.Images.initSlider3}
               heading="REGISTER NOW"
-              subText={Moment(Date.now()).format("DD MMM YYYY")}
-            >
-              Get an extra 3 months warranty when you register your SmoothStar
-              on the app.
+              subText={Moment(Date.now()).format('DD MMM YYYY')}>
+              Get an extra 3 months warranty when you register your SmoothStar on the app.
             </CardRI>
           </View>
         </Content>
@@ -96,34 +90,34 @@ class HomeScreen extends Component {
 const styles = StyleSheet.create({
   carouselViewStyle: {
     height: height / 1.85,
-    position: "absolute",
-    width: "100%",
+    position: 'absolute',
+    width: '100%',
   },
   carouselStyle: {
     flex: 1,
   },
   slideStyle: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#ddd",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ddd',
   },
   imageStyle: {
     height: null,
-    width: "100%",
+    width: '100%',
     flex: 1,
     backgroundColor: Colors.imageBackgroundColor,
   },
   linearGradientStyle: {
     height: height / 1.85,
-    width: "100%",
-    position: "absolute",
+    width: '100%',
+    position: 'absolute',
   },
   cardsViewStyle: {
     // ...GlobalStyles.screenType2ContentStyle,
     paddingHorizontal: 20,
     marginTop: height / 1.85 - 30,
-    width: "100%",
+    width: '100%',
   },
 });
 
